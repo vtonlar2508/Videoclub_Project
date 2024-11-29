0f57c6910e8548029c9de1006daea94d169fc35d
Ffer026 <ffernandogp00@gmail.com>
2024-11-29 17:08:20 +0100

    Update CintaVideo.php

--- Videoclub_Proyect/CintaVideo.php
+++ Videoclub_Proyect/CintaVideo.php
@@ -1,8 +1,12 @@
 <?php
 class CintaVideo extends Soporte {
-        private float $duracion;
+        //CintaVideo es el hijo de Soporte, y por tanto Soporte es el padre de CintaVideo
+        //CintaVideo hereda propiedades y métodos de su padre Soporte, y además puede añadirlos y reescribirlos
+        private float $duracion; //Se añade una nueva propiedad solo de CintaVideo
                         
-        public function MuestraResumen() : void { //Este método no devuelve nada, solo imprime
+        public function muestraResumen() : void { 
+                //El método muestraResumen se sobreescribe al método muestraResumen de la clase padre, sustituyéndolo
+                //Este método no devuelve nada, solo imprime
                 echo "Nombre: " . $this -> titulo . "<br>";
                 echo "Precio base: " . $this -> precio . "<br>";
                 echo "Precio + IVA: " . Soporte::getPrecioConIva() . "<br>";
