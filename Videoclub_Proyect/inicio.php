b0b6859fce5fa2290451d042303de16f05a3f1ca
Ffer026 <ffernandogp00@gmail.com>
2024-11-29 17:13:56 +0100

    Comentarios

--- Videoclub_Proyect/inicio.php
+++ Videoclub_Proyect/inicio.php
@@ -1,8 +1,9 @@
 <?php
-include "Soporte.php";
-$soporte1 = new Soporte("Tenet", 22, 3);
-echo "<strong>" . $soporte1->titulo . "</strong>";
-echo "<br>Precio: " . $soporte1->getPrecio() . " euros";
-echo "<br>Precio IVA incluido: " . $soporte1->getPrecioConIVA() . " euros<br>";
-$soporte1->muestraResumen();
+include "Soporte.php"; //Se le dice al archivo que lea Soporte.php con la clase Soporte para aque pueda usarla
+$soporte1 = new Soporte("Tenet", 22, 3); //Hacemos una nueva variable $soporte1 que es un objeto Soporte
+echo "<strong>" . $soporte1->titulo . "</strong>"; //Imprimimos la variable titulo del objeto soporte1 en negrita
+echo "<br>Precio: " . $soporte1->getPrecio() . " euros"; //Imprimimos la variable precio del objeto soporte1
+echo "<br>Precio IVA incluido: " . $soporte1->getPrecioConIVA() . " euros<br>"; //Imprimimos el resultado del método getPrecioConIVA del objeto soporte1
+//He puesto un salto de línea al final del echo anterior por motivos estéticos
+$soporte1->muestraResumen(); //Ejecutamos la función muestraResumen() del soporte1
 ?>
