0d66d28162d03f4e99fe639289b81de1e3af2c10
Ffer026 <ffernandogp00@gmail.com>
2024-11-29 16:52:42 +0100

    Quitar el HTML

--- Videoclub_Proyect/CintaVideo.php
+++ Videoclub_Proyect/CintaVideo.php
@@ -1,19 +1,12 @@
-<html>
-	<head>
-	<meta charset="UTF-8">
-	</head>
-	<body>
-        <?php
-            class CintaVideo extends Soporte {
-                private float $duracion;
+<?php
+        class CintaVideo extends Soporte {
+        private float $duracion;
                 
-                public function getResumen() : void {
-                    echo "Nombre: " . $this -> nombre;
-                    echo "Precio base: " . Soporte::getPrecio();
-                    echo "Precio + Iva: " . Soporte::getPrecioIva();
-                    echo "Número: " . Soporte::getNumero();
-                }
-            }
-        ?>
-	</body>
-</html>
+        public function getResumen() : void {
+                echo "Nombre: " . $this -> nombre;
+                echo "Precio base: " . Soporte::getPrecio();
+                echo "Precio + Iva: " . Soporte::getPrecioIva();
+        	echo "Número: " . Soporte::getNumero();
+        }
+}
+?>
