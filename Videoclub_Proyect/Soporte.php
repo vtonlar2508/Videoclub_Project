74fe0c588395be640576647e7c1e543795245722
Ffer026 <ffernandogp00@gmail.com>
2024-11-29 16:51:55 +0100

    Quitar HTML

--- Videoclub_Proyect/Soporte.php
+++ Videoclub_Proyect/Soporte.php
@@ -1,39 +1,32 @@
-<html>
-	<head>
-	<meta charset="UTF-8">
-	</head>
-	<body>
-		<?php
-		class Soporte{
-		    public string $titulo;
-		    protected int $numero;
-		    private float $precio;
-		    
-		    public function __construct($titulo, $numero, $precio){
-		        $this -> titulo = $titulo;
-		        $this -> numero = $numero;
-		        $this -> precio = $precio;
-		    }
-		    
-		    public function getPrecio(): float {
-		        return $this -> precio;
-		    }
-		    
-		    public function getPrecioConIVA() : float {
-		        return $this -> precio * 1.21;
-		    }
-		    
-		    public function getNumero() : int {
-		        return $this -> numero;
-		    }
-		    
-		    public function MuestraResumen() : void {
-		        echo "Nombre: " . $this -> titulo . "<br>";
-		        echo "Precio base: " . $this -> precio . "<br>";
-		        echo "Precio + IVA: " . Soporte::getPrecioConIva() . "<br>";
-		        echo "Número: " . $this -> numero;
-		    }
-		}
-		?>
-	</body>
-</html>
+<?php
+class Soporte{
+    public string $titulo;
+    protected int $numero;
+    private float $precio;
+    
+    public function __construct($titulo, $numero, $precio){
+        $this -> titulo = $titulo;
+        $this -> numero = $numero;
+        $this -> precio = $precio;
+    }
+    
+    public function getPrecio(): float {
+        return $this -> precio;
+    }
+    
+    public function getPrecioConIVA() : float {
+        return $this -> precio * 1.21;
+    }
+    
+    public function getNumero() : int {
+        return $this -> numero;
+    }
+    
+    public function MuestraResumen() : void {
+        echo "Nombre: " . $this -> titulo . "<br>";
+        echo "Precio base: " . $this -> precio . "<br>";
+        echo "Precio + IVA: " . Soporte::getPrecioConIva() . "<br>";
+        echo "Número: " . $this -> numero;
+    }
+}
+?>
