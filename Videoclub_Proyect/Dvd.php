053575dd83cd9ca4c1e0c95e2aabdbc70416c0d5
vtonlar2508 <145594215+vtonlar2508@users.noreply.github.com>
2024-12-02 18:17:34 +0100

    Añadido Dvd y inicioDVD

---
+++ Videoclub_Proyect/Dvd.php
@@ -0,0 +1,27 @@
+<?php
+   include "Soporte.php";
+   class Dvd extends Soporte{
+       //La clase Dvd es hija de Soporte
+        public string $idioma;//Se añade las propiedades idioma y...
+        private string $formatPantalla;//... formatPantalla
+        
+        
+        public function __construct(string $titulo, int $numero, float $precio, string $idioma, string $formatPantalla) { 
+            parent::__construct($titulo, $numero, $precio);
+            $this -> idioma = $idioma;
+            $this -> formatPantalla = $formatPantalla;
+            //En el constructor que se ve arriba se ve la creacion y la asigancion de valores de los objetos ... 
+            //... usando propiedades de la clase Soporte y propiedades de la clase Dvd
+        }
+        
+        //En muestraResumen se sobreescriben los resultados de los objetos anterior mencionados...
+        //... el unico objetivo de esta clase es imprimir
+        public function muestraResumen() : void{
+            echo "Pelicula en DVD : <br>" . $this -> titulo . "<br>";
+            echo "Precio base: " . parent::getPrecio() . "<br>";
+            echo "Precio + IVA: " . Soporte::getPrecioConIva() . "<br>";
+            echo "Idioma: " . $this -> idioma . "<br>";
+            echo "Format Pantalla: " . $this -> formatPantalla;
+        }
+    }
+?>
---
+++ Videoclub_Proyect/inicioDVD.php
@@ -0,0 +1,10 @@
+<?php
+include "DVD.php";//Se include los datos del archivo DVD.php a este documento
+
+$miDvd= new Dvd("Origen", 24, 15, "es, es, fr", "16:9");//Con la creacion de la variab miDvd la igualamos al objeto Dvd
+//Con los valores del objeto Dvd mostramos por pantalla las siguientes lineas
+echo "<strong>" . $miDvd->titulo . "</strong>";
+echo "<br>Precio: " . $miDvd->getPrecio() . "euros";
+echo "<br>Precio con IVA incluido: " . $miDvd->getPrecioConIVA() . "euros <br>";
+//Usando los valores del objeto Dvd ejecutamos la funcion muestraResumen
+$miDvd->muestraResumen();
