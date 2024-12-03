2e5f9c8c49903cad0edabe0a7c0acd150e4e7615
Ffer026 <ffernandogp00@gmail.com>
2024-12-03 20:15:45 +0100

    cambiar idioma por idiomas

--- Videoclub_Proyect/Dvd.php
+++ Videoclub_Proyect/Dvd.php
@@ -2,13 +2,13 @@
    include_once "Soporte.php";
    class Dvd extends Soporte{
        //La clase Dvd es hija de Soporte
-        public string $idioma;//Se añade las propiedades idioma y...
+        public string $idiomas;//Se añade las propiedades idiomas y...
         private string $formatPantalla;//... formatPantalla
         
         
-        public function __construct(string $titulo, int $numero, float $precio, string $idioma, string $formatPantalla) { 
+        public function __construct(string $titulo, int $numero, float $precio, string $idiomas, string $formatPantalla) { 
             parent::__construct($titulo, $numero, $precio);
-            $this -> idioma = $idioma;
+            $this -> idiomas = $idiomas;
             $this -> formatPantalla = $formatPantalla;
             //En el constructor que se ve arriba se ve la creacion y la asigancion de valores de los objetos ... 
             //... usando propiedades de la clase Soporte y propiedades de la clase Dvd
@@ -19,7 +19,7 @@
         public function muestraResumen() : void{
             echo "Pelicula en DVD : <br>" ."<I>". $this -> titulo ."</I>". "<br>";
             echo parent::getPrecio() ." ( IVA no incluido )" .  "<br>";
-            echo "Idioma: " . $this -> idioma . "<br>";
+            echo "Idiomas: " . $this -> idiomas . "<br>";
             echo "Format Pantalla: " . $this -> formatPantalla;
         }
     }
