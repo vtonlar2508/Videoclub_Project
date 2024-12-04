7c1f5c5a3b31074ac5f00b455b6f8be85a193cdd
Ffer026 <ffernandogp00@gmail.com>
2024-12-04 11:39:14 +0100

    Error tipográfico

--- Videoclub_Proyect/inicioCintaVideo.php
+++ Videoclub_Proyect/inicioCintaVideo.php
@@ -1,5 +1,5 @@
 <?php
-include "CintaVideo.php"; //Se le dice al archivo que lea CintaVideo.php con la clase CintaVideo para aque pueda usarla
+include "CintaVideo.php"; //Se le dice al archivo que lea CintaVideo.php con la clase CintaVideo para que pueda usarla
 
 $miCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 107); //Hacemos una nueva variable $miCinta que es un objeto de la clase CintaVideo
 echo "<strong>" . $miCinta->titulo . "</strong>"; //Imprimimos la variable titulo del objeto $miCinta en negrita
